9904872dd5b9c00306c16fd2b82c6df637578cc3
weverthonmachado <weverthonmachado@gmail.com>
2015-04-23 23:18:44 -0300

    makeCacheMatrix working

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv<- NULL
+      set <- function(y) {
+            x<<- y
+            inv<<- NULL
+      }
+      get <- function() x
+      setinv<- function(solve) inv<<- solve
+      getinv<- function() inv
+      list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
 
