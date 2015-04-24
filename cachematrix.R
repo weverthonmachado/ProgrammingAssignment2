3610743b68edf6191b5e1d809f8ee42f71cd761b
weverthonmachado <weverthonmachado@gmail.com>
2015-04-24 10:54:54 -0300

    added several comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions make possible to calculate and store the inverse of a 
+## matrix. After the first calculation, the result is saved on the special 
+## matrix object, so it can be just returned whenever needed.
 
-## Write a short comment describing this function
+
+## This function creates a special matrix object, which is actually a list that
+## can hold the supllied matrix and its inverse, after the first calculation.
+## Input is a matrix:
 
 makeCacheMatrix <- function(x = matrix()) {
-      inv<- NULL                 # resets the value of inv
+      inv<- NULL                                  # resets the value of inv
       set <- function(y) {       
-            x<<- y               # changes the value of the x in the main function
-            inv<<- NULL  
+            x<<- y                                # changes the value of 
+            inv<<- NULL                           # the x in the main function
       }
-      get <- function() x        # just prints x
-      setinv<- function(solve) inv<<- solve    # sets inv at whatever 
-                                               # arg the user provides
+      get <- function() x                         # just prints x
+      setinv<- function(solve) inv<<- solve       # sets inv at whatever 
+                                                  # arg the user provides
       
-      getinv<- function() inv                  # prints inv 
-      list(set=set, get=get, setinv=setinv, getinv=getinv) #creates list of functions
+      getinv<- function() inv                     # prints inv 
+      list(set=set, get=get, 
+           setinv=setinv, 
+           getinv=getinv)                         #creates list of functions
 }
 
 
-## Write a short comment describing this function
+## This function first checks if there's already a saved inverse on the special
+## matrix object. If there's not, it calculates and saves it. Input is the 
+## special matrix object.
 
 cacheSolve <- function(x, ...) {
-      inv <-x$getinv()
-      if(!is.null(inv)){
+      inv <-x$getinv()                         # assigns the value of inv 
+                                               # contained in x
+      if(!is.null(inv)){ 
             message("no need to calculate again! let me get the cached data...")
-            return(inv)
-      }
-      data<-x$get()
-      inv<-solve(data)
-      x$setinv(inv)
-      inv      
+            return(inv)                        # if there's saved value of inv,
+                  }                            # doesn't cal again and return it
+      
+      data<-x$get()                            # gets the original matrix    
+      inv<-solve(data)                         # inverts the matrix
+      x$setinv(inv)                            # saves the inverse matrix in x
+      inv                                      # print inv
 }
